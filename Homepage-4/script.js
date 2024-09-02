9e7f1501b56dd19e93bedd38fb326741e00b8778
santosh-techmarcos <santoshkumar@techmarcos.com>
2024-09-02 22:29:35 +0530

    add final template 4

--- Homepage-4/script.js
+++ Homepage-4/script.js
@@ -1,2 +1,20 @@
 new FlipDown(1588017373, "registerBy").start();
 new FlipDown(1593561600, "eventStart").start();
+
+
+var swiper = new Swiper(".mySwiper", {
+  direction: "vertical",
+  slidesPerView: 3,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+$(document).ready(function() {
+  $("#accordion").accordion({
+      icons: false, // Disable default jQuery UI icons
+      heightStyle: "content"
+  });
+});
