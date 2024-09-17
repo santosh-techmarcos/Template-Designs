2b2857d8c379735b9795411436437b5ecb1907bc
santosh-techmarcos <santoshkumar@techmarcos.com>
2024-09-17 18:37:08 +0530

    Modifiy One on one template

---
+++ One-on-One sessions/script.js
@@ -0,0 +1,28 @@
+$('.whatsapp-slider').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  nextArrow: '#Whatsapp-next',
+  prevArrow: '#Whatsapp-prev',
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
