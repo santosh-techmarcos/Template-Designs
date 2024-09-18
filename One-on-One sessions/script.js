a4bca7574cf475c3c2838b948b85a2385067ffe9
santosh-techmarcos <santoshkumar@techmarcos.com>
2024-09-18 09:12:21 +0530

    update

--- One-on-One sessions/script.js
+++ One-on-One sessions/script.js
@@ -26,3 +26,37 @@ $('.whatsapp-slider').slick({
     // instead of a settings object
   ]
 });
+// Logo slider
+$('.logo-slider').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  arrows:false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 3,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+      }
+    }
+  ]
+});
